52d64edd39d21289be8943f5832b94297e93d9f6
Matt <choplin.mat@gmail.com>
2018-11-04 00:49:08 +0000

    Progress on course

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+mkdir venv && cd venv && python3 -m venv .
+source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install --upgrade ipython jupyter RISE
+cd ..
+jupyter-nbextension install rise --py --sys-prefix
+jupyter-nbextension enable rise --py --sys-prefix
+jupyter notebook
+
